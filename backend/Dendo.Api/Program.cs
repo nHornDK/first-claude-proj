c9ee6d9fde1dfea67b3c8f66c93b7ff780d8ea0d
NHornDK <mikkel@nhorn.dk>
2026-03-04 03:31:54 +0100

    loig

--- backend/Dendo.Api/Program.cs
+++ backend/Dendo.Api/Program.cs
@@ -11,6 +11,7 @@ using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Logging.ClearProviders().AddConsole();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(
@@ -87,13 +88,13 @@ using (var scope = app.Services.CreateScope())
     if (db.Database.IsRelational()) db.Database.Migrate();
 
     var users = scope.ServiceProvider.GetRequiredService<IUserRepository>();
-    //var logger = scope.ServiceProvider.GetRequiredService<ILogger>();
+    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
     db.Users.ToList().ForEach(u => {
-        Console.WriteLine(JsonSerializer.Serialize(u));
+        logger.LogInformation($"Users:  {JsonSerializer.Serialize(u)}");
         db.Users.Remove(u);
         });
     db.SaveChanges();
-    Console.WriteLine(JsonSerializer.Serialize(app.Configuration));
+    logger.LogInformation($"app.Configuration: {JsonSerializer.Serialize(app.Configuration)}");
     if (!await users.AnyAsync())
     {
         var demoUser = app.Configuration.GetSection("DemoUser");
