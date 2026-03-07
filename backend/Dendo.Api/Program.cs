aa0a4f8bf8eef37e66243e4b7d5f07f4e2e955c7
NHornDK <mikkel@nhorn.dk>
2026-03-07 21:03:24 +0100

    Add conditional check for database migration in Program.cs

--- backend/Dendo.Api/Program.cs
+++ backend/Dendo.Api/Program.cs
@@ -84,7 +84,8 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    await db.Database.MigrateAsync();
+    if (db.Database.IsRelational())
+        await db.Database.MigrateAsync();
 
     //using (var scope = app.Services.CreateScope())
     //{
